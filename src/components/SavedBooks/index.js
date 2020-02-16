86ffef29d322ce800f851bbdcff3b302d38a2787
Ari Tserendavaa <tserenda@hotmail.com>
2020-02-16 12:32:03 -0800

    applied the search book style to the saved books

--- src/components/SavedBooks/index.js
+++ src/components/SavedBooks/index.js
@@ -3,11 +3,13 @@ import Book from "../Book";
 
 export default function SavedBooks(props) {
   return (
-    <div className="SavedBooks">
-      <h3>Saved Books</h3>
-      {props.books.map((book, i) => (
-        <Book key={i} renderedBy="SavedBooks" action={props.action} book={book} />
-      ))}
+    <div className="card col-6 mx-auto SavedBooks">
+      <div className="card-header font-weight-bolder">Saved Books</div>
+      <div className="card-body">
+        {props.books.map((book, i) => (
+          <Book key={i} renderedBy="SavedBooks" action={props.action} book={book} />
+        ))}
+      </div>
     </div>
   );
 }
